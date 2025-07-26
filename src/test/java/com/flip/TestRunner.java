b526de5b2fff59dc848f277a85a191873eab5024
Surya97rpm <pandimahasurya6@gmail.com>
2025-07-26 09:23:46 +0530

    script updated with 2 new class

---
+++ src/test/java/com/flip/TestRunner.java
@@ -0,0 +1,11 @@
+package com.flip;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith (Suite.class)
+    @Suite.SuiteClasses({flipkartLaunch.class,TvPurchase.class})
+
+public class TestRunner {
+
+}
---
+++ src/test/java/com/flip/TvPurchase.java
@@ -0,0 +1,100 @@
+package com.flip;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class TvPurchase {
+	static WebDriver driver;
+	 static long start;
+
+	    @BeforeClass
+	    public static void browserlaunch() {
+	        System.setProperty("webdriver.edge.driver", "C:\\Users\\User\\eclipse-workspace\\FlipkartLaunch\\target\\msedgedriver.exe");
+	        driver = new EdgeDriver();
+	        driver.manage().window().maximize();
+	        System.out.println("Edge browser launched");
+	    }
+
+	    @AfterClass
+	    public static void quit() {
+	           driver.quit();
+	            System.out.println("Edge browser closed");
+	        }
+	    
+	    @Before
+	    public void starttime() {
+	    	start= System.currentTimeMillis();
+	    	System.out.println("before");
+	    }
+	    @After
+	    public void endtime() {
+	    	long end= System.currentTimeMillis();
+	    	System.out.println("Aftertotaltime"+ (end-start));
+	    }
+	    @Test
+	    public void method1() throws InterruptedException {
+	        driver.get("https://www.flipkart.com/");
+	        Thread.sleep(2000);
+	      
+	    }
+	    @Test
+	    public void method2() throws InterruptedException, AWTException {
+	    	WebElement search=driver.findElement(By.xpath("//input[@type='text']"));
+	    	search.sendKeys("Realme mobile");
+	    	 Thread.sleep(2000);
+	    	 Robot r= new Robot();
+	    		r.keyPress(KeyEvent.VK_ENTER);
+	    	    r.keyRelease(KeyEvent.VK_ENTER);
+	    	    Thread.sleep(2000);
+	    }
+	    @Test
+	    public void method3() {
+	    	WebElement getproduct = driver.findElement(By.xpath("//div[@class='KzDlHZ']"));
+        	getproduct.click();
+	    }
+	    @Test
+	    public void method4() {
+	    	String parentwindow=driver.getWindowHandle();
+	        
+	        Set<String>allwindow=driver.getWindowHandles();
+	        
+	        for(String x:allwindow) {
+	        	if(!parentwindow.equals(x)) {
+	        		driver.switchTo().window(x);
+	        	}
+	        }
+	       WebElement text= driver.findElement(By.xpath("//span[contains(text(),'realme C61 (Safari Green, 128 GB)')]"));
+	       System.out.println(text.getText());
+	    }
+	    @Test
+	    public void method5() {
+	        WebElement text = driver.findElement(By.xpath("//span[contains(text(),'realme C61')]"));
+	        String ProductName = text.getText().toLowerCase();
+	        if (ProductName.contains("realme c61")) {
+	            System.out.println("method 5= validation pass");
+	        } else {
+	            System.out.println("method 5= validation fail");
+	        }
+	    }
+	    @Ignore
+	    @Test
+	    public void method6() {
+	    	WebElement buynow = driver.findElement(By.xpath("//button[@class='QqFHMw vslbG+ _3Yl67G _7Pd1Fp']"));
+	    	buynow.click();
+	    	
+	    }
+}
+
